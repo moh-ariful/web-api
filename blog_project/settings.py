f62ed35e1f496d729402af43a09cee87a5186b47
Moh Ariful <nextkoding@gmail.com>
2022-06-23 08:34:28 +0700

    Web API sdh bisa diakses di http://127.0.0.1:8000/api/v1/

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {  # new
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# posts/views.py
+from rest_framework import generics
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
